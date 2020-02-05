d6fbc62ec5a6474265c8c72e9005d4bf004c8e73
PutilovDmitriy <dmitrii.98.putilov@gmai.com>
2020-02-05 13:51:55 +0300

    minor changes

--- app/Controllers/SurnameController.js
+++ app/Controllers/SurnameController.js
@@ -5,22 +5,12 @@ Ext.define('TestApp.Controllers.SurnameController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.surname',
 
-    /**
-     * Called when the view is created
-     */
-    init: function () {
-
-    },
-
     onSave: function () {
-
         var form = Ext.getCmp('surnamePanel'),
             active = form.activeRecord;
-
         if (!active) {
             return;
         }
-
         if (form.isValid()) {
             form.updateRecord(active);
             this.onReset();
